6a784a51c3517f7e1d6f40966b20ed6014ffc874
Andrew Liu <andrewlliu@gmail.com>
2019-04-14 07:15:33 -0700

    ALMOST DONE

--- background.js
+++ background.js
@@ -12,32 +12,36 @@
 //       }]);
 //     });
 //   });
+var EVEN_API_KEY = 'e7675dd3-ff3b-434b-95aa-70251cc3784b_88140dd4-f13e-4ce3-8322-6eaf2ee9a2d2';
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-      if (request.contentScriptQuery == 'loanOffers') {
+        console.log('hi')
         //EVEN API!!!!
-        var lead = '{ "productTypes": [ "loan", "savings" ], "personalInformation": { "firstName": "John", "lastName": "Doe", "email": "john@example.com", "city": "New York", "state": "NY", "workPhone": "2125551234", "primaryPhone": "2125556789", "address1": "45 West 21st Street", "address2": "5th Floor", "zipcode": "10010", "monthsAtAddress": 5, "driversLicenseNumber": "111222333", "driversLicenseState": "NY", "ipAddress": "8.8.8.8", "activeMilitary": false, "militaryVeteran": true, "dateOfBirth": "1993-10-09", "educationLevel": "bachelors", "ssn": "111-22-3333" }, "loanInformation": { "purpose": "home_refi", "loanAmount": 10000 }, "mortgageInformation": { "propertyType": "condo", "propertyStatus": "own_with_mortgage", "propertyValue": 200000, "mortgageBalance": 10000, "lenderName": "Bank OF NY", "hasFHALoan": true, "currentWithLoan": true }, "creditCardInformation": { "allowAnnualFee": true, "cardBenefits": [ "travel_incentives" ] }, "creditInformation": { "providedCreditRating": "excellent", "providedNumericCreditScore": 750 }, "financialInformation": { "employmentStatus": "employed", "employmentPayFrequency": "weekly", "annualIncome": 120000, "monthlyNetIncome": 10000, "bankName": "Santander", "bankRoutingNumber": "231372691", "bankAccountType": "savings", "monthsAtBank": 10, "bankAccountNumber": "1234567890" }, "employmentInformation": { "employerName": "EVEN Financial", "employerAddress": "45 W 21st St", "employerCity": "New York", "employerState": "NY", "employerZip": "10010", "jobTitle": "Software Engineer", "monthsEmployed": 14, "directDeposit": true, "payDate1": "2004-10-06", "payDate2": "2004-11-06" }, "legalInformation": { "consentsToFcra": true, "consentsToTcpa": true, "tcpaLanguage": "I agree to be contacted by Even Financial and its partners at the telephone number(s) I have provided above to explore personal loan offers, including contact through automatic dialing systems, artificial or pre-recorded voice messaging, or text message. I understand my consent is not required as a condition to purchasing any goods or services from anyone." }, "clientTags": { "hello": [ "world", "there" ], "something": [ "else" ] } }'
+        var lead = '{ "productTypes": [ "loan" ], "personalInformation": { "firstName": "John", "lastName": "Doe", "email": "john@example.com", "zipcode": "98006", "ipAddress": "8.8.8.8", "dateOfBirth": "1993-10-09", "educationLevel": "bachelors" }, "loanInformation": {}, "creditInformation": { "providedNumericCreditScore": 600 }, "financialInformation": { "employmentStatus": "employed", "monthlyNetIncome": 7000 }, "legalInformation": { "consentsToFcra": true, "consentsToTcpa": true, "tcpaLanguage": "I agree to be contacted by Even Financial and its partners at the telephone number(s) I have provided above to explore personal loan offers, including contact through automatic dialing systems, artificial or pre-recorded voice messaging, or text message. I understand my consent is not required as a condition to purchasing any goods or services from anyone." } }'
         var myHeaders = new Headers({
             'Authorization': 'Bearer ' + EVEN_API_KEY,
             'Accept': 'application/vnd.evenfinancial.v1+json',
             'Content-Type': 'application/json'
         });
-        const request = new Request('https://api.evenfinancial.com/leads/rateTables', {method: 'POST', body: lead, headers: myHeaders, mode: 'cors'});
-        fetch(request)
+        const requestAPI = new Request('https://api.evenfinancial.com/leads/rateTables', {method: 'POST', body: lead, headers: myHeaders, mode: 'cors'});
+        fetch(requestAPI)
         .then(response => {
-            console.log(response)
             return response.json()
         })
         .then(response => {
-            console.log(response)
+            sendMessage(response)
             // ...
         }).catch(error => {
             console.error(error);
         });
         return true;  // Will respond asynchronously.
-      }
     });
 
-chrome.runtime.sendMessage(
-        {contentScriptQuery: 'queryPrice', itemId: 12345},
-        price => ...);
+function sendMessage(response) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {response: response}, function(response) {
+            return true;
+        });
+    });
+}
--- content.js
+++ content.js
@@ -1,18 +1,19 @@
+var evenResponse;
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log("content.js got a message")
-        console.log(request);
-        console.log(sender);
+        evenResponse = request.response
+        console.log(evenResponse)
+        return true;
     }
 );
 
 chrome.runtime.sendMessage({contentScriptQuery: "loanOffers"}, function(response) {
-    console.log(response.farewell);
+    return true;
 });
 
 //alert('test')
 var price
-var EVEN_API_KEY = 'e7675dd3-ff3b-434b-95aa-70251cc3784b_88140dd4-f13e-4ce3-8322-6eaf2ee9a2d2';
 
 if (document.getElementById('priceblock_ourprice')) {
     price = document.getElementById('priceblock_ourprice').innerHTML
@@ -50,10 +51,49 @@ topBanner.innerHTML = 'find an applicable loan with EVEN'
 topBanner.style.cursor = 'pointer';
 topBanner.onclick = function() {
     //append item for each loan offer
-    var loanOffersDiv = document.createElement('div')
-    loanOffersDiv.id = 'loanOffers'
+    var loanOffersTitle = document.createElement('div')
+    loanOffersTitle.innerHTML = 'Loan Offers'
+    loanOffersTitle.id = 'loanOffersTitle'
+    dealsPopUp.appendChild(loanOffersTitle)
+
+    var loanOffersTable = document.createElement('table')
+    loanOffersTable.id = 'loanOffersTable'
+
+    var loanOffersTr = document.createElement('tr')
+    loanOffersTr.id = 'loanOffersTr'
+    var td1 = document.createElement('td')
+    td1.innerHTML = 'Provider'
+    var td2 = document.createElement('td')
+    td2.innerHTML = 'Amount'
+    var td3 = document.createElement('td')
+    td3.innerHTML = 'APR'
+    var td4 = document.createElement('td')
+    td4.innerHTML = 'Term Length'
+    loanOffersTr.appendChild(td1)
+    loanOffersTr.appendChild(td2)
+    loanOffersTr.appendChild(td3)
+    loanOffersTr.appendChild(td4)
+
+    loanOffersTable.appendChild(loanOffersTr)
+    for (var i=0;i<evenResponse.loanOffers.length;i++) {
+        loanOffersTr = document.createElement('tr')
+        var provider = document.createElement('td')
+        provider.innerHTML = evenResponse.loanOffers[i].originator.name
+        var amount = document.createElement('td')
+        amount.innerHTML = evenResponse.loanOffers[i].maxAmount
+        var APR = document.createElement('td')
+        APR.innerHTML = evenResponse.loanOffers[i].meanApr
+        var termLength = document.createElement('td')
+        termLength.innerHTML = evenResponse.loanOffers[i].termLength + ' ' + evenResponse.loanOffers[i].termUnit + '(s)'
+        
+        loanOffersTr.appendChild(provider)
+        loanOffersTr.appendChild(amount)
+        loanOffersTr.appendChild(APR)
+        loanOffersTr.appendChild(termLength)
+        loanOffersTable.appendChild(loanOffersTr)
+    }
     //loanOffersDiv.innerHTML = evenResponse.uuid
-    dealsPopUp.appendChild(loanOffersDiv)
+    dealsPopUp.appendChild(loanOffersTable)
 
     document.body.appendChild(dealsPopUp)
 };

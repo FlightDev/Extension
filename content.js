d3049238d3110a9ec28bbf708c044ffb5d660430
Andrew Liu <andrewlliu@gmail.com>
2019-04-14 06:30:00 -0700

    working on api

--- content.js
+++ content.js
@@ -1,3 +1,15 @@
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log("content.js got a message")
+        console.log(request);
+        console.log(sender);
+    }
+);
+
+chrome.runtime.sendMessage({contentScriptQuery: "loanOffers"}, function(response) {
+    console.log(response.farewell);
+});
+
 //alert('test')
 var price
 var EVEN_API_KEY = 'e7675dd3-ff3b-434b-95aa-70251cc3784b_88140dd4-f13e-4ce3-8322-6eaf2ee9a2d2';
@@ -9,17 +21,6 @@ if (document.getElementById('priceblock_ourprice')) {
 }
 
 document.getElementById('productTitle').innerHTML = 'terrance li ' + price
-
-// .then(response => {
-//     console.log(response)
-//     return response.json()
-// })
-// .then(response => {
-//     console.log(response)
-//     // ...
-// }).catch(error => {
-//     console.error(error);
-// });
 
 //deals popup
 var dealsPopUp = document.createElement("div")
@@ -48,23 +49,13 @@ topBanner.id = 'topBanner'
 topBanner.innerHTML = 'find an applicable loan with EVEN'
 topBanner.style.cursor = 'pointer';
 topBanner.onclick = function() {
-    //EVEN API!!!!
-    var lead = '{ "productTypes": [ "loan", "savings" ], "personalInformation": { "firstName": "John", "lastName": "Doe", "email": "john@example.com", "city": "New York", "state": "NY", "workPhone": "2125551234", "primaryPhone": "2125556789", "address1": "45 West 21st Street", "address2": "5th Floor", "zipcode": "10010", "monthsAtAddress": 5, "driversLicenseNumber": "111222333", "driversLicenseState": "NY", "ipAddress": "8.8.8.8", "activeMilitary": false, "militaryVeteran": true, "dateOfBirth": "1993-10-09", "educationLevel": "bachelors", "ssn": "111-22-3333" }, "loanInformation": { "purpose": "home_refi", "loanAmount": 10000 }, "mortgageInformation": { "propertyType": "condo", "propertyStatus": "own_with_mortgage", "propertyValue": 200000, "mortgageBalance": 10000, "lenderName": "Bank OF NY", "hasFHALoan": true, "currentWithLoan": true }, "creditCardInformation": { "allowAnnualFee": true, "cardBenefits": [ "travel_incentives" ] }, "creditInformation": { "providedCreditRating": "excellent", "providedNumericCreditScore": 750 }, "financialInformation": { "employmentStatus": "employed", "employmentPayFrequency": "weekly", "annualIncome": 120000, "monthlyNetIncome": 10000, "bankName": "Santander", "bankRoutingNumber": "231372691", "bankAccountType": "savings", "monthsAtBank": 10, "bankAccountNumber": "1234567890" }, "employmentInformation": { "employerName": "EVEN Financial", "employerAddress": "45 W 21st St", "employerCity": "New York", "employerState": "NY", "employerZip": "10010", "jobTitle": "Software Engineer", "monthsEmployed": 14, "directDeposit": true, "payDate1": "2004-10-06", "payDate2": "2004-11-06" }, "legalInformation": { "consentsToFcra": true, "consentsToTcpa": true, "tcpaLanguage": "I agree to be contacted by Even Financial and its partners at the telephone number(s) I have provided above to explore personal loan offers, including contact through automatic dialing systems, artificial or pre-recorded voice messaging, or text message. I understand my consent is not required as a condition to purchasing any goods or services from anyone." }, "clientTags": { "hello": [ "world", "there" ], "something": [ "else" ] } }'
-    var myHeaders = new Headers({
-        'Authorization': 'Bearer ' + EVEN_API_KEY,
-        'Accept': 'application/vnd.evenfinancial.v1+json',
-        'Content-Type': 'application/json'
-    });
-    const request = new Request('https://api.evenfinancial.com/leads/rateTables', {method: 'POST', body: lead, headers: myHeaders, mode: 'cors'});
-
-    fetch(request)
     //append item for each loan offer
-    // var loanOffersDiv = document.createElement('div')
-    // loanOffersDiv.id = 'loanOffers'
-    // loanOffersDiv.innerHTML = evenResponse.uuid
-    // dealsPopUp.appendChild(loanOffersDiv)
+    var loanOffersDiv = document.createElement('div')
+    loanOffersDiv.id = 'loanOffers'
+    //loanOffersDiv.innerHTML = evenResponse.uuid
+    dealsPopUp.appendChild(loanOffersDiv)
 
-    // document.body.appendChild(dealsPopUp)
+    document.body.appendChild(dealsPopUp)
 };
 document.body.appendChild(topBanner); 
 
